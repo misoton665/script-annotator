cc3282ccf1c89ea05451c810f0ee4185b0bd107e
MPrain <mapurgina@gmail.com>
2019-06-29 21:22:52 +0900

    bug fixes

--- src/static/annotators/intonationfinal.js
+++ src/static/annotators/intonationfinal.js
@@ -6,7 +6,8 @@ function myfunc(sentence) {
     return sentence;
   }
 
-  var R = ['Do', 'Does', 'Did', 'Is', 'Are', 'Was', 'Were', 'Can', 'Would', 'Could', 'Shall', 'Should', 'Must', 'Oh'];
+  var R = ['Do', 'Does', 'Did', 'Is', 'Are', 'Was', 'Were', 'Can', 'Would', 'Could', 'Shall', 'Should', 'Must', 'Oh', 'Will', 'Have', 'Had', ' Has'];
+  var P = ['Dr', 'Jr', 'Mr', 'Mrs', 'Miss', 'Ms', 'Prof'];
   var bun = sentence;
   bunlen = 0;
   tmp = "";
@@ -14,32 +15,47 @@ function myfunc(sentence) {
   for (i = 0; i < bun.length; i++) {
     flg = 0;
     if (bun[i] == '.' || bun[i] == '?' || bun[i] == '!') {
+		
+		
       flg = 2;
       sen = bun.slice(bunlen, i + 1);
-      bunlen = i + 1;
-      tmp += sen;
-
-      var tango = sen.split(" ");
+	  var tango = sen.split(/\W+/);	  
       len = sen.length;
-      for (j = 0; j < R.length; j++)
-        if (R[j] == tango[0] && (sen[len - 1] == '?' || sen[len - 1] == '!')) flg = 1;
-      if (tango[0] == 'May' && sen[len - 1] == '?') flg = 1;
-
-      for (j = 0; j < tango.length; j++)
-        if (tango[j] == 'or' && (sen[len - 1] == '? ' || sen[len - 1] == '.')) flg = 2;
-
-      if (flg == 1) tmp += ' ⤴️ ';
-      else if (flg == 2) tmp += ' ⤵️ ';
+	
+	  var fist = tango[0];
+	  	
+	  for (j = 0; j < tango.length; j++){
+		  if (fist <= 1) fist = tango[j];
+		  if (tango[j] == 'or' && (sen[len - 1] == '?' || sen[len - 1] == '.')) flg = 2;
+		  
+	  }
+	  
+      for (j = 0; j < R.length; j++){		
+		if (fist == R[j] && (sen[len - 1] == '?' || sen[len - 1] == '!')) flg = 1;
+		if (fist == 'May' && sen[len - 1] == '?') flg = 1;} 
+  
+	for (j = 0; j < tango.length; j++){
+		  
+		  for (p = 0; p < P.length; p++){
+			if (P[p] == tango[j]) flg = 3;}
+	  }
+	   
+	  bunlen = i + 1;
+      tmp += sen;
+	  if (flg == 1) tmp += ' ⤴️ ';
+	   else if (flg == 2) tmp += ' ⤵️ ';
+	   else if (flg == 3) tmp += '';
+	  
     }
   }
 
   if (flg == 0) tmp += bun.slice(bunlen);
 
-	tmp = tmp.replace(/(\sand)(\W*)/gi, ' ⤴️ and$2'); 
-	tmp = tmp.replace(/(\sbut)(\W*)/gi, ' ⤴️ but$2');
-	tmp = tmp.replace(/(\sso)(\W*)/gi, ' ⤴️ so$2');
-	tmp = tmp.replace(/(\sbecause)(\W*)/gi, ' ⤴️ because$2');
-	tmp = tmp.replace(/(\sor)(\W*)/gi, ' ⤴️ or$2');
+	tmp = tmp.replace(/(\sand\s)(\W*)/gi, ' ⤴️ and $2'); 
+	tmp = tmp.replace(/(\sbut\s)(\W*)/gi, ' ⤴️ but $2');
+	tmp = tmp.replace(/(\sso\s)(\W*)/gi, ' ⤴️ so $2');
+	tmp = tmp.replace(/(\sbecause\s)(\W*)/gi, ' ⤴️ because $2');
+	tmp = tmp.replace(/(\sor\s)(\W*)/gi, ' ⤴️ or $2');
   
   return tmp;
 
